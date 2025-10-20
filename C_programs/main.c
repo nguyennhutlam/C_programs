544601aaf676b16d1011234c64ce9488fcf8b698
nguyennhutlam <lamnn@tvu.edu.vn>
2025-10-20 16:00:14 +0700

    Initial Commit

---
+++ C_programs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_programs
+//
+//  Created by Nhut-Lam Nguyen on 20/10/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
